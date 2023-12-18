158d4fb787951cc7eeb062a333aa73bafbbafcca
nachooosss <jmrg5364@gmail.com>
2023-12-18 11:39:05 -0500

    add iconmenu and make responsive

--- src/components/Home/Header/Nav/Nav.jsx
+++ src/components/Home/Header/Nav/Nav.jsx
@@ -1,8 +1,9 @@
 "use client";
 import AdbIcon from "@mui/icons-material/Adb";
-import { Grid, AppBar, Typography, Button } from "@mui/material";
+import { Grid, AppBar, Typography, Button, Box, SvgIcon } from "@mui/material";
 import "./Nav.css";
 import { Bebas_Neue } from "next/font/google";
+import MenuIcon from "@/assets/SVG/menuIcon.svg";
 
 const inter = Bebas_Neue({ weight: ["400"], subsets: ["latin"] });
 
@@ -14,17 +15,34 @@ function Nav() {
     "Nosotros",
     "Contacto",
   ];
+  const styles = (theme) => ({
+    root: {
+      backgroundColor: "blue",
+      [theme.breakpoints.between("xs", "md")]: {
+        backgroundColor: "red",
+      },
+    },
+  });
+
   return (
     <AppBar className="navbar">
       <Grid container>
-        <Grid className="grid-navbar" item xs={5}>
+        <Grid className="grid-navbar" item lg={5} xs={4}>
           <AdbIcon className="logo" />
           <Typography className={inter.className + " txt-Logo"} variant="h3">
             Media Holding
           </Typography>
         </Grid>
-
-        <Grid className="grid-btn" item xs={7}>
+        <Box
+          component={Grid}
+          className="grid-btn"
+          item
+          lg={7}
+          display={{
+            xs: "none !important",
+            lg: "flex !important",
+          }}
+        >
           {pages.map((page) => (
             <Button
               key={page}
@@ -34,7 +52,23 @@ function Nav() {
               {page}
             </Button>
           ))}
-        </Grid>
+        </Box>
+        <Box
+          component={Grid}
+          className="grid-btn"
+          item
+          xs={8}
+          display={{
+            xs: "flex-end !important",
+            lg: "none !important",
+          }}
+        >
+          <SvgIcon
+            component={MenuIcon}
+            sx={{ fontSize: 35, marginInline: "40px", color: "white" }}
+            inheritViewBox
+          />
+        </Box>
       </Grid>
     </AppBar>
   );
