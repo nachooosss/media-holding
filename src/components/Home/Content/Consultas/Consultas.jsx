a926b66bf2af1315e79bdd0505aee8b5ea2d956e
nachooosss <jmrg5364@gmail.com>
2023-12-27 09:54:36 -0500

    update consultas

--- src/components/Home/Content/Consultas/Consultas.jsx
+++ src/components/Home/Content/Consultas/Consultas.jsx
@@ -17,181 +17,101 @@ const inter = Bebas_Neue({ weight: ["400"], subsets: ["latin"] });
 
 export default function Consultas() {
   return (
-    <Grid
-      container
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "start",
-        mt: 15,
-      }}
-    >
-      <Grid item xs={4} sx={{ mx: 4 }}>
-        <Card sx={{ mt: -5, boxShadow: 10 }}>
-          <CardActionArea>
-            <CardContent sx={{ backgroundColor: orange[400], color: "white" }}>
-              <Typography
-                className={inter.className}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "start",
-                }}
-                gutterBottom
-                variant="h3"
-                component="div"
-              >
-                Agenda una cita
-              </Typography>
-              <Typography
-                className={inter.className}
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "start",
-                }}
-                variant="body2"
-              >
-                Lizards are a widespread group of squamate
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Grid
-              container
+    <Card sx={{ mb: 5, boxShadow: 0, borderRadius: "40px" }}>
+      <CardActions>
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            p: 5,
+          }}
+        >
+          <Grid itme xs={4}>
+            <Typography
+              // className={inter.className}
               sx={{
                 display: "flex",
                 justifyContent: "center",
-                alignItems: "center",
-                px: 8,
+                alignItems: "start",
+                fontWeight: "bold",
               }}
+              gutterBottom
+              variant="h6"
+              component="div"
             >
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  my: 2,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <TextField
-                  className={inter.className}
-                  id="outlined-basic"
-                  label="Introduce tu nombre"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  my: 2,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <TextField
-                  id="outlined-basic"
-                  label="Introduce tu correo electrónico"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  my: 2,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <TextField
-                  id="outlined-basic"
-                  label="Asunto"
-                  variant="outlined"
-                  fullWidth
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  my: 2,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <TextField
-                  label="Mensaje"
-                  placeholder="Mensaje"
-                  multiline
-                  fullWidth
-                  rows={6}
-                />
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                sx={{
-                  my: 2,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <Button
-                  className={inter.className}
-                  variant="contained"
-                  sx={{
-                    backgroundColor: orange[400],
-                    color: "white",
-                    paddingInline: 5,
-                  }}
-                  size="medium"
-                >
-                  Enviar
-                </Button>
-              </Grid>
-            </Grid>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item xs={6} sx={{ mx: 4 }}>
-        <Typography
-          className={inter.className}
-          gutterBottom
-          variant="h6"
-          component="div"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          Consultas
-        </Typography>
-        <Typography
-          className={inter.className}
-          gutterBottom
-          variant="h3"
-          component="div"
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          Preguntas más frecuentes
-        </Typography>
-        <ExpandQuestions></ExpandQuestions>
-      </Grid>
-    </Grid>
+              AGENDA UNA CITA:
+            </Typography>
+          </Grid>
+          <Grid
+            item
+            xs={8}
+            sx={{
+              display: "flex",
+              justifyContent: "right",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              label="Introduce tu nombre"
+              variant="outlined"
+              sx={{
+                width: "95%",
+              }}
+              InputProps={{
+                style: {
+                  borderRadius: "40px",
+                },
+              }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              my: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Introduce tu nombre"
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                style: {
+                  borderRadius: "40px",
+                },
+              }}
+            />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              my: 1,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <TextField
+              id="outlined-basic"
+              label="Introduce un mensaje"
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                style: {
+                  borderRadius: "40px",
+                },
+              }}
+            />
+          </Grid>
+        </Grid>
+      </CardActions>
+    </Card>
   );
 }
